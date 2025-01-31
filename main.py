5c332fd436740cbb20f38eb4d41251c2d69b0da2
Emil <emilbjorn101@gmail.com>
2025-01-31 14:38:46 +0100

    commit

---
+++ main.py
@@ -0,0 +1,54 @@
+import sys
+
+import pygame
+
+from scripts.utils import load_image
+from scripts.entities import PhysicsEntity
+
+class Game:
+    def __init__(self):
+        pygame.init()
+
+        pygame.display.set_caption('test game')
+
+        RESOLUTION = (640, 480)
+        self.screen = pygame.display.set_mode(RESOLUTION)
+
+        self.clock = pygame.time.Clock()
+        
+
+        self.movement = [False, False]
+        self.assets = {
+            'player' : load_image('data\images\entities\player.png')
+        }
+
+        self.player = PhysicsEntity(self, 'player', (50,50), (8,15))
+
+    def run(self):
+        while True:
+            self.screen.fill((14, 219, 248))
+            #Rendering is based on order
+            self.player.update((self.movement[1] - self.movement[0], 0))
+            self.player.render(self.screen)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_w:
+                        self.movement[0] = True
+                    if event.key == pygame.K_s:
+                        self.movement[1] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_w:
+                        self.movement[0] = False
+                    if event.key == pygame.K_s:
+                        self.movement[1] = False
+
+            pygame.display.update()
+            self.clock.tick(60) #60 fps - fixed frame rate
+
+Game().run()
+
+
